f2eb4bbdd691c44b9355b90f0561646753aa8997
Sheldon Hart <sheldon.hart88@gmail.com>
2025-02-23 15:16:48 +0200

    Enhance project list component with GitHub repository data and add sitemap and robots modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: "https://www.fromthehart.tech",
+    name: "My frontend sandbox to play with tech",
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  typescript: {
-    typeCheck: true,
-  },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/sitemap", "@nuxtjs/robots"],
   future: {
     compatibilityVersion: 4,
+  },
+  routeRules: {
+    "/": { prerender: true },
+    "/about/**": { prerender: true },
+    "/blog/**": { prerender: true },
+    "/projects/**": { ssr: false },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/sitemap.xml", "/robots.txt"],
+    },
   },
 });
