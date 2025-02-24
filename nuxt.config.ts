a63d381c7e182d5279b38786943339a2349c4843
Sheldon Hart <sheldon.hart88@gmail.com>
2025-02-24 11:44:03 +0200

    Add cache-control headers to S3 sync and remove redundant headers from Nuxt route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,34 +11,17 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   routeRules: {
-    "/_nuxt/**": {
-      headers: {
-        "cache-control": "public, s-maxage=2629800, max-age=6",
-      },
-    },
     "/": {
       prerender: true,
-      headers: {
-        "cache-control": "public, s-maxage=2629800, max-age=6",
-      },
     },
     "/about/**": {
       prerender: true,
-      headers: {
-        "cache-control": "public, s-maxage=2629800, max-age=6",
-      },
     },
     "/blog/**": {
       prerender: true,
-      headers: {
-        "cache-control": "public, s-maxage=2629800, max-age=6",
-      },
     },
     "/projects/**": {
       ssr: false,
-      headers: {
-        "cache-control": "public, s-maxage=2629800, max-age=6",
-      },
     },
   },
 });
