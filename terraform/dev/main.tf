534684157298b0b9537206e130704b2d0bdeced7
Sheldon Hart <sheldon.hart88@gmail.com>
2025-04-14 14:10:41 +0200

    Add ACM certificate ARN to website module configuration

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,6 +1,7 @@
 module "website" {
   source              = "../modules/s3_and_cloudfront_static_website"
   domain_name         = "dev.fromthehart.tech"
+  acm_certificate_arn = data.terraform_remote_state.shared.outputs.acm_certificate_arn
   tags = {
     Domain      = "tech"
     Project     = "from-the-hart-tech-website"
